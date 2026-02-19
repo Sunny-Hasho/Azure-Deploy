22983cd3a7056b29fad1dceea9b00f802f273295
Sunny-Hasho <sandeep.hasarinda@gmail.com>
2026-02-19 10:19:42 +0530

    Initial commit

---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentService.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+        
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+    }
+}
